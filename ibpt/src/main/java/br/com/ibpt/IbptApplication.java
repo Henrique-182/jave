8f030bfd96fb2baebc7b142bd438f6e0d71e8903
Henrique Lobo <henriqueaugustolobo.dev@gmail.com>
2023-09-11 11:16:26 -0300

    Configuração Inicial do Spring com Dependências
    Spring Boot Starter, Mysql Connector, Flyway, Spring JPA, MapStruct, Application.YML

---
+++ ibpt/src/main/java/br/com/ibpt/IbptApplication.java
@@ -0,0 +1,13 @@
+package br.com.ibpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbptApplication.class, args);
+	}
+
+}
