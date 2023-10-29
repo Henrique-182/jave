76a4dd9aeb723bc89f0dc6a7a930b652888f0d43
Henrique Lobo <96430157+Henrique-182@users.noreply.github.com>
2023-10-29 15:01:46 -0300

    Swagger for UserController

--- ibpt/src/main/java/br/com/ibpt/controllers/v1/UserController.java
+++ ibpt/src/main/java/br/com/ibpt/controllers/v1/UserController.java
@@ -14,29 +14,95 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.ibpt.data.vo.v1.AccountCredentialsVO;
 import br.com.ibpt.data.vo.v1.UserVO;
 import br.com.ibpt.services.v1.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/v1/usuario")
+@Tag(name = "User", description = "Endpoints for Managing Users")
 public class UserController {
 	
 	@Autowired
 	private UserService userService;
 	
+	@Operation(
+		summary = "Finds a User",
+		description = "Finds a User By Id",
+		tags = {"User"},
+		responses = {
+			@ApiResponse(description = "Success", responseCode = "200", content = @Content(schema = @Schema(implementation = UserVO.class))),
+			@ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Forbidden", responseCode = "403", content = @Content),
+			@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Interval Server Error", responseCode = "500", content = @Content),
+		}
+	)
 	@GetMapping("/{id}")
 	public UserVO findById(@PathVariable("id") Integer id) {
 		return userService.findById(id);
 	}
 	
+	@Operation(
+		summary = "Finds all Users",
+		description = "Finds all Users",
+		tags = {"User"},
+		responses = {
+			@ApiResponse(
+				description = "Success", 
+				responseCode = "200", 
+				content = @Content(
+					mediaType = "application/json",
+					array = @ArraySchema(schema = @Schema(implementation = UserVO.class))
+				)
+			),
+			@ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Forbidden", responseCode = "403", content = @Content),
+			@ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+			@ApiResponse(description = "Interval Server Error", responseCode = "500", content = @Content),
+		}
+	)
 	@GetMapping
 	public List<UserVO> findAll() {
 		return userService.findAll();
 	}
 	
+	@Operation(
+		summary = "Creates a User",
+		description = "Creteas a User",
+		tags = {"User"},
+		responses = {
+			@ApiResponse(description = "Created", responseCode = "200", content = @Content(schema = @Schema(implementation = UserVO.class))),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Forbidden", responseCode = "403", content = @Content),
+			@ApiResponse(description = "Interval Server Error", responseCode = "500", content = @Content),
+		}
+	)
 	@PostMapping("/novo")
 	public UserVO create(@RequestBody AccountCredentialsVO vo) {
 		 return userService.create(vo);
 	}
 	
+	@Operation(
+		summary = "Updates a User",
+		description = "Updates a User",
+		tags = {"User"},
+		responses = {
+			@ApiResponse(description = "Success", responseCode = "200", content = @Content(schema = @Schema(implementation = UserVO.class))),
+			@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+			@ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+			@ApiResponse(description = "Forbidden", responseCode = "403", content = @Content),
+			@ApiResponse(description = "Interval Server Error", responseCode = "500", content = @Content),
+		}
+	)
 	@PutMapping("/{id}")
 	public UserVO updateById(@PathVariable("id") Integer id, @RequestBody UserVO vo) {
 		return userService.updateById(id, vo);
