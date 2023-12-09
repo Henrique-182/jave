437eaf54f3e0fd5575085a137afae203a88d1791
Henrique Lobo <96430157+Henrique-182@users.noreply.github.com>
2023-12-09 12:03:04 -0300

    Adicionado Self Signed Certificate

--- ibpt/src/main/java/br/com/ibpt/IbptApplication.java
+++ ibpt/src/main/java/br/com/ibpt/IbptApplication.java
@@ -11,24 +11,6 @@ public class IbptApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IbptApplication.class, args);
-		
-		/*
-		Map<String, PasswordEncoder> encoders = new HashMap<>();
-
-		encoders.put("pbkdf2", new Pbkdf2PasswordEncoder("", 8, 185000, SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256));
-
-		DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
-
-		passwordEncoder.setDefaultPasswordEncoderForMatches(new Pbkdf2PasswordEncoder("", 8, 185000, SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256));
-
-
-
-		String result = passwordEncoder.encode("he@01");
-		String result1 = passwordEncoder.encode("ri@01");
-
-		System.out.println("My hash " + result);
-		System.out.println("My hash " + result1);
-        */
 	}
 
 }
