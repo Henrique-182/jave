247fd9fdc3c992c30d90d70aa884ff31166f053b
Henrique Lobo <96430157+Henrique-182@users.noreply.github.com>
2023-12-23 09:25:29 -0300

    Migrations V9 até V14, Entities e Repository
    V9__CREATE_USERS
    V10__INSERT_USERS
    V11__CREATE_PERMISSION
    V12__INSERT_PERMISSION
    V13__CREATE_USERPERMISSION
    V14__INSERT_USERPERMISSION

---
+++ conhecimento/src/main/java/br/com/conhecimento/model/v2/User.java
@@ -0,0 +1,185 @@
+package br.com.conhecimento.model.v2;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "USERS")
+public class User implements Serializable, UserDetails {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID", nullable = false)
+	private Integer id;
+	
+	@Column(name = "USER_NAME", nullable = false)
+	private String username;
+	
+	@Column(name = "FULL_NAME", nullable = false)
+	private String fullname;
+	
+	@Column(name = "PASSWORD", nullable = false)
+	private String password;
+	
+	@Column(name = "ACCOUNT_NON_EXPIRED", nullable = false)
+	private Boolean accountNonExpired;
+	
+	@Column(name = "ACCOUNT_NON_LOCKED", nullable = false)
+	private Boolean accountNonLocked;
+	
+	@Column(name = "CREDENTIALS_NON_EXPIRED", nullable = false)
+	private Boolean credentialsNonExpired;
+	
+	@Column(name = "ENABLED", nullable = false)
+	private Boolean enabled;
+	
+	@ManyToMany
+	@JoinTable(
+			name = "USER_PERMISSION",
+			joinColumns = @JoinColumn(name = "FK_USER"),
+			inverseJoinColumns = @JoinColumn(name = "FK_PERMISSION")
+	)
+	private List<Permission> permissions;
+
+	public User() {}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return this.permissions;
+	}
+
+	@Override
+	public String getPassword() {
+		return this.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		return this.getUsername();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return this.accountNonExpired;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return this.accountNonLocked;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return this.credentialsNonExpired;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return this.enabled;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFullname() {
+		return fullname;
+	}
+
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+
+	public Boolean getAccountNonExpired() {
+		return accountNonExpired;
+	}
+
+	public void setAccountNonExpired(Boolean accountNonExpired) {
+		this.accountNonExpired = accountNonExpired;
+	}
+
+	public Boolean getAccountNonLocked() {
+		return accountNonLocked;
+	}
+
+	public void setAccountNonLocked(Boolean accountNonLocked) {
+		this.accountNonLocked = accountNonLocked;
+	}
+
+	public Boolean getCredentialsNonExpired() {
+		return credentialsNonExpired;
+	}
+
+	public void setCredentialsNonExpired(Boolean credentialsNonExpired) {
+		this.credentialsNonExpired = credentialsNonExpired;
+	}
+
+	public Boolean getEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(Boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public List<Permission> getPermissions() {
+		return permissions;
+	}
+
+	public void setPermissions(List<Permission> permissions) {
+		this.permissions = permissions;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(accountNonExpired, accountNonLocked, credentialsNonExpired, enabled, fullname, id, password,
+				permissions, username);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(accountNonExpired, other.accountNonExpired)
+				&& Objects.equals(accountNonLocked, other.accountNonLocked)
+				&& Objects.equals(credentialsNonExpired, other.credentialsNonExpired)
+				&& Objects.equals(enabled, other.enabled) && Objects.equals(fullname, other.fullname)
+				&& Objects.equals(id, other.id) && Objects.equals(password, other.password)
+				&& Objects.equals(permissions, other.permissions) && Objects.equals(username, other.username);
+	}
+	
+}
---
+++ conhecimento/src/main/java/br/com/conhecimento/repositories/v2/UserRepository.java
@@ -0,0 +1,14 @@
+package br.com.conhecimento.repositories.v2;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import br.com.conhecimento.model.v2.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+	User findByUsername(@Param("username") String username);
+	
+}
