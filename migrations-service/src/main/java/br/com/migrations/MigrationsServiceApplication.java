c485149272b5f812a105d2a7441ef1491ab5be75
Henrique Lobo <96430157+Henrique-182@users.noreply.github.com>
2024-01-11 16:47:53 -0300

    Criado Serviço Migrations

---
+++ migrations-service/src/main/java/br/com/migrations/MigrationsServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.migrations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigrationsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigrationsServiceApplication.class, args);
+	}
+
+}
---
+++ migrations-service/src/test/java/br/com/migrations/MigrationsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.migrations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigrationsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
